e1d10fc4370d9b97e53f778f17a63a109067c861
Ben Zotto <bzotto@gmail.com>
2014-12-08 13:20:36 -0800

    Create NSURL+DataAdditions.m

---
+++ NSURL+DataAdditions.m
@@ -0,0 +1,24 @@
+//
+//  NSURL+DataAdditions.m
+//
+//  Created by Ben Zotto.
+//  Public domain. Knock yourself out.
+//
+
+#import "NSURL+DataAdditions.h"
+
+@implementation NSURL (DataAdditions)
++ (instancetype)URLWithData:(NSData *)data
+{
+    return [self URLWithData:data mime:@"application/octet-stream"];
+}
+
++ (instancetype)URLWithData:(NSData *)data mime:(NSString *)mime
+{
+    mime = mime ?: @"application/octet-stream";
+    NSString * base64 = [data base64EncodedStringWithOptions:0];
+    NSString * urlString = [NSString stringWithFormat:@"data:%@;base64,%@", mime, base64];
+    base64 = nil; // Force this possibly-large temp string to get deallocated.
+    return [self URLWithString:urlString];
+}
+@end
